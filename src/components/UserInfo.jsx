02e141c014f6deb2cf84c8ee3d2cb997d6df2759
Venus <venusyip@outlook.com>
2025-03-25 05:11:50 +0000

    Edit avatar.png and add to user info section

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,6 +1,11 @@
 export default function UserInfo() {
   return (
     <section className='user-info'>
+      <img
+        src={require('../assets/images/avatar.png')}
+        alt='Avatar'
+        className='user-info__avatar'
+      />
       <h1 className='user-info__name'>Venus Yip</h1>
       <p className='user-info__location'>Essex, United Kingdom</p>
       <p className='user-info__description'>
