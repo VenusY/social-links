13415186e5a8a4c6c1c932916fa26c0b710f58af
Venus <venusyip@outlook.com>
2025-03-25 18:28:41 +0000

    Add target attribute to anchor tags

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,21 +3,28 @@ export default function Links() {
     <div className='links-section'>
       <a
         href='https://venusy.github.io/portfolio/'
+        target='_blank'
         className='links-section__link'
       >
         My website
       </a>
-      <a href='https://github.com/VenusY' className='links-section__link'>
+      <a
+        href='https://github.com/VenusY'
+        target='_blank'
+        className='links-section__link'
+      >
         GitHub
       </a>
       <a
         href='https://www.frontendmentor.io/profile/VenusY'
+        target='_blank'
         className='links-section__link'
       >
         Frontend Mentor
       </a>
       <a
         href='https://www.linkedin.com/in/venus-yip-869aa4217/'
+        target='_blank'
         className='links-section__link'
       >
         LinkedIn
